93dafdde298501c39992a9674199ad386594f6f1
unknown <joaquin.blanco@bootcamp-upgrade.com>
2024-08-28 17:39:53 +0200

    Solo falta acabar la pagina de proyectos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { InfoCardComponent } from './components/pages/cards/info-card/info-card.component';
 import { StackCardComponent } from './components/pages/cards/stack-card/stack-card.component';
-import { SoftSkillCardComponent } from './components/pages/cards/soft-skill-card/soft-skill-card.component';
 import { StudiesCardComponent } from './components/pages/cards/studies-card/studies-card.component';
 import { ProyectsCardComponent } from './components/pages/cards/proyects-card/proyects-card.component';
 import { HobbiesCardComponent } from './components/pages/cards/hobbies-card/hobbies-card.component';
@@ -20,10 +19,6 @@ export const routes: Routes = [
     {
         path: "stack",
         component: StackCardComponent,
-    },
-    {
-        path: "softskills",
-        component:SoftSkillCardComponent,
     },
     {
         path: "studies",
