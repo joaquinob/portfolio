85dfcf6580a5668c5b6b4a10f239fab747040200
Joaquin Ortega <joaquin.blanco@bootcamp-upgrade.com>
2024-08-28 21:02:31 +0200

    swal del correo

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  email: string = 'joaquin280401@gmail.com';
 
+  copyEmail() {
+    // Crea un campo de texto temporal para copiar el texto
+    const tempInput = document.createElement('input');
+    tempInput.value = this.email;
+    document.body.appendChild(tempInput);
+
+    // Selecciona el texto y copia al portapapeles
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); // Para dispositivos móviles
+    document.execCommand('copy');
+
+    // Remueve el campo de texto temporal
+    document.body.removeChild(tempInput);
+
+    // Opcional: muestra un mensaje de confirmación al usuario
+    Swal.fire('Correo copiado al portapapeles: ' + this.email);
+  }
 }
